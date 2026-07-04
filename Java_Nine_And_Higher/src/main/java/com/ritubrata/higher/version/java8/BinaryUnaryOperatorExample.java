8c01c13f2fb4747e0fa0a34652c6872eaeaf4043
ritubrata91 <ritubrata91@gmail.com>
2026-07-04 09:25:41 +0530

    implement logger implementaion

--- Java_Nine_And_Higher/src/main/java/com/ritubrata/higher/version/java8/BinaryUnaryOperatorExample.java
+++ Java_Nine_And_Higher/src/main/java/com/ritubrata/higher/version/java8/BinaryUnaryOperatorExample.java
@@ -1,25 +1,29 @@
 package com.ritubrata.higher.version.java8;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.function.BinaryOperator;
 import java.util.function.UnaryOperator;
 
 public class BinaryUnaryOperatorExample {
+	private static final Logger LOGGER = LoggerFactory.getLogger(BinaryUnaryOperatorExample.class);
 
+	static void main(final String[] args) {
 
-	public static void main(final String[] args) {
 		//Binary Operator example
 		final BinaryOperator<Integer> add = (a, b) -> a + b;
-		System.out.println("add 10 + 25: " + add.apply(10, 25));
+		LOGGER.info("add 10 + 25: " + add.apply(10, 25));
 
 		//Unary Operator example
 		final UnaryOperator<String> str  = String::toUpperCase;
-		System.out.println(str.apply("This is my message in upper case"));
+		LOGGER.info(str.apply("This is my message in upper case"));
 
 		//these two are the same using the static method concat
 		final UnaryOperator<String> greeting = "Hello, "::concat;
-		System.out.println(greeting.apply("World"));
+		LOGGER.info(greeting.apply("World"));
 
 		final UnaryOperator<String> makeGreeting = "Hello, "::concat;
-		System.out.println(makeGreeting.apply("Peggy"));
+		LOGGER.info(makeGreeting.apply("Peggy"));
 	}
 
 }
